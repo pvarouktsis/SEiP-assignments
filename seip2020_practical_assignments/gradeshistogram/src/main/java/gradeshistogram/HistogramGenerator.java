a369c835bc90b8edb8fb68db8683da5b71dc8117
pvarouktsis <p.varouktsis@gmail.com>
2020-03-28 12:06:26 +0200

    Fix #3

--- seip2020_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ seip2020_practical_assignments/gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -1,33 +1,63 @@
 package gradeshistogram;
 
 import java.io.*;
-import java.net.*;
+import org.jfree.chart.*;
+import org.jfree.data.statistics.*;
+import org.jfree.chart.plot.*;
 import java.util.ArrayList;
 
 public class HistogramGenerator {
 
-    public HistogramGenerator() {}
-
-    public double[] readGradesFromFile(String fileName) throws FileNotFoundException, IOException {               
-        ClassLoader cl = getClass().getClassLoader();
-        URL rsc = cl.getResource(fileName);
-        File file = new File(rsc.getFile());
-        //System.out.println("File Found: " + file.getName());
-        
+    public static double[] readGradesFromFile(String filePath) {               
         ArrayList<Integer> grades = new ArrayList<Integer>();
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String num;
-        while((num = br.readLine()) != null) {
-            grades.add(Integer.parseInt(num));
-        } 
-         
+        try {
+            File file = new File(filePath);
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String num;
+            while((num = br.readLine()) != null) {
+                grades.add(Integer.parseInt(num));
+            } 
+            br.close();
+        } catch (Exception e) {
+            System.out.println("There is no such file.");
+        }
         //from ArrayList<Integer> to double[]
-        double[] gradesArray = new double[grades.size()];
+        final double[] gradesArray = new double[grades.size()];
         for(int i = 0; i < grades.size(); i++) {
             gradesArray[i] = grades.get(i);
         }
         return gradesArray;
+    }
 
+    public static void generateChart(double[] gradesArray) {
+        HistogramDataset dataset = new HistogramDataset();
+        dataset.setType(HistogramType.FREQUENCY);
+        dataset.addSeries("grades", gradesArray, 35);
+
+        boolean legend = false;
+        boolean tooltips = false;
+        boolean urls = false;
+        JFreeChart chart = ChartFactory.createXYLineChart(
+            "Students' Grades Histogram",   //title
+            "Grades",                       //x_axis
+            "Frequency",                    //y_axis
+            dataset, 
+            PlotOrientation.VERTICAL, 
+            legend, 
+            tooltips, 
+            urls
+        );
+        ChartFrame frame = new ChartFrame("Histogram", chart);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        String filePath = args[0];
+        double[] grades = readGradesFromFile(filePath);
+        //double[] grades = {3, 4, 5, 7, 0, 9, 6, 2, 0, 0, 2, 8, 0, 0, 4};
+        generateChart(grades);
     }
 
 }
