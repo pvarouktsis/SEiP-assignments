b436bfda512b798cfca68ffaf60ea4a4e5256eae
pvarouktsis <p.varouktsis@gmail.com>
2020-04-19 01:29:50 +0300

    Update test cases for MyMath

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -3,10 +3,16 @@ package math;
 public class MyMath {
 
   public int factorial(int x) {
-    if (x == 0) {
-      return 1;
+    if (x < 0) {
+      throw new IllegalArgumentException("Expected x >= 0");
+    } else if (x > 12) {
+      throw new IllegalArgumentException("Integer output out of bounds");
     } else {
-      return x * factorial(x-1);
+      if (x == 0) {
+        return 1;
+      } else {
+        return x * factorial(x-1);
+      }
     }
   }
 
