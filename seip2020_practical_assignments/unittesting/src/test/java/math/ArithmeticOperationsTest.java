5a48e1d473886636c09de5e63f1f171f5075b7d1
pvarouktsis <p.varouktsis@gmail.com>
2020-04-18 23:01:26 +0300

    Update test cases for ArithmeticOperations

--- seip2020_practical_assignments/unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -10,12 +10,42 @@ public class ArithmeticOperationsTest {
 
   @Test
   public void testDivide() {
+    //
     Assert.assertEquals(2.0, arops.divide(10.0, 5.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(0.0, 5.0), 0.0001);
+    Assert.assertEquals(3.3333, arops.divide(10.0, 3.0), 0.0001);
+    //
+    Assert.assertEquals(-2.0, arops.divide(10.0, -5.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(0.0, -5.0), 0.0001);
+    Assert.assertEquals(-3.3333, arops.divide(10.0, -3.0), 0.0001);
+    //
+    Assert.assertEquals(-2.0, arops.divide(-10.0, 5.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(-0.0, 5.0), 0.0001);
+    Assert.assertEquals(-3.3333, arops.divide(-10.0, 3.0), 0.0001);
+    //
+    Assert.assertEquals(2.0, arops.divide(-10.0, -5.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(-0.0, -5.0), 0.0001);
+    Assert.assertEquals(3.3333, arops.divide(-10.0, -3.0), 0.0001);
+    //
+    Assert.assertEquals(0.0, arops.divide(0.0, 1.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(0.0, -1.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(-0.0, 1.0), 0.0001);
+    Assert.assertEquals(0.0, arops.divide(-0.0, -1.0), 0.0001);
+
   }
   
   @Test (expected = ArithmeticException.class)
   public void testDivideExceptiion() {
     arops.divide(10.0, 0.0);
+  }
+
+  @Test
+  public void testMultiply() {
+    Assert.assertEquals(1, arops.multiply(1, 1));
+    Assert.assertEquals(98, arops.multiply(1, 98));
+    Assert.assertEquals(1376515, arops.multiply(587, 2345));
+    Assert.assertEquals(0, arops.multiply(0, 91));
+    //TODO test for y = 0 but y is divided by the MAX_VALUE
   }
 
   @Rule
