b2d55a45b9322c55f93a7b844da3f9a6adaedcba
pvarouktsis <p.varouktsis@gmail.com>
2020-04-20 00:18:22 +0300

    Add javadoc in MyMath

--- seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
+++ seip2020_practical_assignments/unittesting/src/main/java/math/MyMath.java
@@ -1,7 +1,23 @@
 package math;
 
+/**
+ * MyMath provides a simple recursive method that
+ * finds the factorial of a number in the interval [0, 12].
+ * @author Panagiotis Varouktsis
+ * @version 1.0
+ * @since 2020-04-19
+ */
 public class MyMath {
 
+  /**
+   * Performs recursive calls, decreasing each
+   * time the input by 1, until 0, where it returns 1.
+   * Finally returns, recursively, the product of them.
+   * @param x the input
+   * @return the factorial of the x
+   * @exception IllegalArgumentException when x < 0
+   * @exception IllegalArgumentException when x > 12
+   */
   public int factorial(int x) {
     if (x < 0) {
       throw new IllegalArgumentException("Expected x >= 0");
