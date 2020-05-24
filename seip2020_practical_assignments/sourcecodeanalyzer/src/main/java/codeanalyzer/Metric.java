53d69aafb4fe7c4d976fc7f482a6e5d9bc8d1087
pvarouktsis <p.varouktsis@gmail.com>
2020-05-24 11:59:18 +0300

    Implement Metric, interface for different source code metrics

---
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/Metric.java
@@ -0,0 +1,8 @@
+package codeanalyzer;
+
+import java.util.List;
+
+interface Metric {
+  public int calculateWithRegex(String sourceCodeString);
+  public int calculateWithStrcomp(List<String> sourceCodeList);
+}
