9c2676cd675f68fb892dbb573568a83a6d6e3c9f
pvarouktsis <p.varouktsis@gmail.com>
2020-05-25 13:09:35 +0300

    Update DemoClient

--- seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/demo/DemoClient.java
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/demo/DemoClient.java
@@ -7,19 +7,18 @@ import java.util.Map;
 import codeanalyzer.*;
 
 public class DemoClient {
-
 	public static void main(String[] args) throws IOException {
-		String filepath = "src/main/resources/TestClass.java";
+		String sourceFilepath = "src/main/resources/TestClass.java";
 		String sourceCodeAnalyzerType = "regex";
 		String sourceFileLocation = "local";
-		String outputFilePath = "output_metrics";
+		String outputFilepath = "output_metrics";
 		String outputFileType = "csv";
 		
 		if(args.length == 5) {
-			filepath = args[0];
+			sourceFilepath = args[0];
 			sourceCodeAnalyzerType = args[1];
 			sourceFileLocation = args[2];
-			outputFilePath = args[3];
+			outputFilepath = args[3];
 			outputFileType = args[4];
 		} else if (args.length != 0) {
 			System.out.println("Incorrect number of arguments.");
@@ -27,12 +26,11 @@ public class DemoClient {
 		}
 
 		// main code
-		SourceCodeAnalyzer analyzer = new SourceCodeAnalyzer(sourceFileLocation);
 		Map<String, Integer> metrics = new HashMap<String, Integer>();
-		metrics = analyzer.calculateMetrics(filepath, sourceCodeAnalyzerType);
-				
-		MetricsExporter exporter = new MetricsExporter();
-		exporter.writeFile(outputFileType, metrics, outputFilePath);
+		SourceCodeAnalyzer analyzer = new SourceCodeAnalyzer(sourceFilepath, sourceFileLocation, sourceCodeAnalyzerType);
+		MetricsExporter exporter = new MetricsExporter(outputFilepath, outputFileType, metrics);
+		metrics = analyzer.calculateMetrics();
+		exporter.exportMetrics();
 	}
 
 }
