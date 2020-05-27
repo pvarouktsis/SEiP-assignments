4004546ac08a7cf0d987d90346bbae69e8e7e5b8
pvarouktsis <p.varouktsis@gmail.com>
2020-05-27 18:03:43 +0300

    Add Javadoc in Analyzer

--- seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/Analyzer.java
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/Analyzer.java
@@ -8,12 +8,13 @@ import java.util.Map;
 /**
  * Analyzes the contents of a Java source code file 
  * and calculates the following metrics: loc = lines of code,
- * nom = number of methods, and noc=number of classes. 
+ * nom = number of methods, and noc = number of classes. 
  * The current functionality supports two types of source code analysis,
  * namely regex (with the use of regular expressions) and 
  * strcomp (with the use of string comparison).
  * @author agkortzis
- *
+ * @author pvarouktsis
+ * 
  */
 public class Analyzer {
 	protected String sourceFilepath;
@@ -22,6 +23,15 @@ public class Analyzer {
 	protected String outputFilepath;
 	protected String outputFileType;
 
+	/**
+	 * Initializes the necessary instances for
+	 * analyzer.
+	 * @param sourceFilepath, the path of the Java file
+	 * @param sourceFileLocation, web or local
+	 * @param sourceCodeAnalyzerType, regex or strcomp
+	 * @param outputFilepath, the path of the output file
+	 * @param outputFileType, the format of the output file
+	 */
 	public Analyzer(String sourceFilepath, String sourceFileLocation, String sourceCodeAnalyzerType, String outputFilepath, String outputFileType) {
 		this.sourceFilepath = sourceFilepath;
 		this.sourceFileLocation = sourceFileLocation;
@@ -30,6 +40,13 @@ public class Analyzer {
 		this.outputFileType = outputFileType;
 	}
 
+	/**
+	 * Calculates source code metrics (loc, nom, noc) based on 
+	 * the sourceCodeAnalyzerType, regex or strcomp. 
+	 * @return a Map that contains the name and the value of
+	 * each metric analyzed
+	 * @throws IOException
+	 */
 	public Map<String, Integer> calculateMetrics() throws IOException {
 		SourceFileReaderFactory sfrf = new SourceFileReaderFactory();
 		SourceFileReader sfr = sfrf.initializeSourceFileReader(sourceFilepath, sourceFileLocation);
@@ -58,12 +75,22 @@ public class Analyzer {
 		return metrics;
 	}
 
+	/**
+	 * Exports source code metrics based on the outputFilepath
+	 * and outputFileType.
+	 * @param metrics, the metrics (loc, nom, noc) to export
+	 */
 	public void exportMetrics(Map<String, Integer> metrics) {
 		ExporterFactory ef = new ExporterFactory();
 		Exporter ex = ef.initializeExporter(outputFilepath, outputFileType);
 		ex.write(metrics);
 	}
 	
+	/**
+	 * Calls calculateMetrics and exportMetrics to
+	 * analyze the source code and export the results.
+	 * @throws IOException
+	 */
 	public void execute() throws IOException {
 		exportMetrics(calculateMetrics()); // calculate and export metrics
 	}
