accd1804e8f79cc4ad8a48a17e4e954fdb743038
pvarouktsis <p.varouktsis@gmail.com>
2020-05-31 22:12:03 +0300

    Update travis, rename test.sh to test_sca.sh

--- test/test_sca.sh
+++ test/test_sca.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Initialization of variables
 TEST_FILE='verify_outputs.csv'
 
+# Produce outputs.csv
 java -jar ./../seip2020_practical_assignments/sourcecodeanalyzer/target/sourcecodeanalyzer-1.0-SNAPSHOT-jar-with-dependencies.jar \
 ./../seip2020_practical_assignments/sourcecodeanalyzer/src/test/resources/TestClass.java \
 regex \
@@ -9,11 +11,13 @@ local \
 outputs \
 csv
 
+# Produce manually verify_outputs.csv
 echo "LOC,NOC,NOM," > $TEST_FILE
 echo "21,3,3," >> $TEST_FILE
 echo "Manually test file created successfully"
 echo ""
 
+# Test
 if ["$(diff outputs.csv $TEST_FILE)" == ""]
 then
   echo "[PASSED] Integration Test of Source Code Analyzer"
@@ -21,5 +25,7 @@ else
   echo "[FAILED] Integration Test of Source Code Analyzer"
 fi
 
+# Delete produced files
 rm outputs.csv
 rm $TEST_FILE
+
